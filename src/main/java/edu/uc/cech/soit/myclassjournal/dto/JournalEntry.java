76143b18446c8146762e971b67202f2ce3cde7de
Kenny <keotero99@gmail.com>
2023-09-25 18:30:06 -0400

    Added JavaDoc comments.

--- src/main/java/edu/uc/cech/soit/myclassjournal/dto/JournalEntry.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/dto/JournalEntry.java
@@ -2,6 +2,9 @@ package edu.uc.cech.soit.myclassjournal.dto;
 
 import lombok.Data;
 
+/**
+ * DTO for Journals
+ */
 public @Data
 class JournalEntry {
     private String notes;
--- src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
@@ -6,15 +6,27 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Creates the ability to save and get all journals.
+ */
 @Service
 public class JournalService implements IJournalService {
+    // Store journals in an arraylist
     List<JournalEntry> journal = new ArrayList<>();
 
+    /**
+     * Save a journal into our arraylist.
+     * @param journalEntry the entry to save.
+     */
     @Override
     public void save(JournalEntry journalEntry) {
         journal.add(journalEntry);
     }
 
+    /**
+     * Get all journals
+     * @return the arraylist containing all journals
+     */
     @Override
     public List<JournalEntry> fetchAll() {
         return journal;
